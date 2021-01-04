34823a0f5bf8b2ffc1185224b8623fe51a156897
dudtj051292 <65510280+dudtj051292@users.noreply.github.com>
2021-01-04 18:09:16 +0900

    lombok 삭제 / gitignore 추가

--- GWD_LMS_SYS/src/main/java/com/min/edu/vo/AuthVo.java
+++ GWD_LMS_SYS/src/main/java/com/min/edu/vo/AuthVo.java
@@ -2,19 +2,59 @@ package com.min.edu.vo;
 
 import org.springframework.security.core.GrantedAuthority;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.ToString;
 
-@Data
-@AllArgsConstructor
-@ToString
 public class AuthVo  implements GrantedAuthority{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String auth;
 	private String info;
+	
+	public AuthVo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public AuthVo(String auth, String info) {
+		super();
+		this.auth = auth;
+		this.info = info;
+	}
+
+
+	
+	public String getAuth() {
+		return auth;
+	}
+
+
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+
+
+	public String getInfo() {
+		return info;
+	}
+
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+
+	@Override
+	public String toString() {
+		return "AuthVo [auth=" + auth + ", info=" + info + "]";
+	}
+
+
 	@Override
 	public String getAuthority() {
 		// TODO Auto-generated method stub
 		return this.auth;
 	}
+	
+	
 }
--- GWD_LMS_SYS/src/main/java/com/min/edu/vo/LoginVo.java
+++ GWD_LMS_SYS/src/main/java/com/min/edu/vo/LoginVo.java
@@ -1,16 +1,45 @@
 package com.min.edu.vo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.ToString;
 
-@Data
-@ToString
-@AllArgsConstructor
 public class LoginVo {
 	private String id;
 	private String password;
 	private String enabled;
+	public LoginVo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public LoginVo(String id, String password, String enabled) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.enabled = enabled;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(String enabled) {
+		this.enabled = enabled;
+	}
+	@Override
+	public String toString() {
+		return "LoginVo [id=" + id + ", password=" + password + ", enabled=" + enabled + "]";
+	}
 	
 	
 }
