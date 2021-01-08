7c44efcc502a40d8a502581570fcaa16058f94c2
dudtj051292 <65510280+dudtj051292@users.noreply.github.com>
2021-01-08 17:53:03 +0900

    selectOneUser_dynamic 다이나믹 쿼리 작성

--- GWD_LMS_SYS/src/main/java/com/min/edu/service/IServiceUser.java
+++ GWD_LMS_SYS/src/main/java/com/min/edu/service/IServiceUser.java
@@ -1,6 +1,7 @@
 package com.min.edu.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,4 +24,6 @@ public interface IServiceUser extends UserDetailsService {
 	public List<StudentDto> selectAllUser();
 	
 	public StudentDto selectOneUser(String id);
+
+	public List<StudentDto> selectOneUser_dynamic(Map<String, String> map);
 }
--- GWD_LMS_SYS/src/main/java/com/min/edu/service/ServiceUserImpl.java
+++ GWD_LMS_SYS/src/main/java/com/min/edu/service/ServiceUserImpl.java
@@ -3,6 +3,7 @@ package com.min.edu.service;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.slf4j.Logger;
@@ -131,6 +132,11 @@ public class ServiceUserImpl implements IServiceUser {
 	public StudentDto selectOneUser(String id) {
 		// TODO Auto-generated method stub
 		return dao.selectOneUser(id);
+	}
+	@Override
+	public List<StudentDto> selectOneUser_dynamic(Map<String, String>map) {
+		// TODO Auto-generated method stub
+		return dao.selectOneUser_dynamic(map);
 	}
 	
 	
